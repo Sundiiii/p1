f54838da4c827012526989adcc367199a3b339c2
sundar sanjay deshmukh <sundardeshmuk01@gmail.com>
2023-11-09 08:26:36 +0530

    rama

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import React, { useState } from 'react'
 import axios from 'axios';
 //import Search1 from './search';
 function App() {
-  const [dataapi,setdataapi]=useState({ name:"",temp:"",main:""});
+  const [dataapi,setdataapi]=useState({ name:"",temp:"",main:"",description:""});
   const [search,setsearch]=useState("");
-  const  apiKey= "840de593b7028de6e424162454790fe5";
-  //const uri= `https://api.openweathermap.org/data/2.5/weather?q=[CITY_NAME]&units=metric&appid=[840de593b7028de6e424162454790fe5]`
-  const data1=  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=[${search}]&units=metric&appid=[${apiKey}]`)
+  const  APIKey= "840de593b7028de6e424162454790fe5";
+  const data1=  axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${APIKey}`)
     .then((response) => {
       if (response.data) {
-      console.log(response.data);
-        setdataapi({      
-       name: response.main.name,
-          temp: response.main.temp,
-           main: response.clouds,
-        })
+     //console.log(response.data,"response...");
+      setdataapi({      
+        name: response.data.name,
+        temp: response.data.main.temp,
+        main: response.data.weather[0].main,
+        description: response.data.weather[0].description,
+      })
+    console.log(dataapi.name,"name");
+      //console.log(dataapi.temp,"temp");
+       //console.log(dataapi.main,"main");
+      //console.log(dataapi.description,"desc..");
        
       }
     })
@@ -24,10 +28,8 @@ function App() {
       console.log(error.message);   
     });
     
-   
     const handleClick = ()=>{
-      let popup = document.querySelector('.popup-message') ;
-      popup.style.disply = 'none';
+      setsearch("") ;
     }
     const clearData = ()=>{
       setsearch("") ;
@@ -41,7 +43,7 @@ function App() {
       <form onSubmit={data1}>
         <div>
         <input type='text' placeholder='search' value={search} onChange={(e)=>setsearch(e.target.value)}/>
-        <button onClick={clearData}>x</button>
+        <button onClick={clearData}>clear</button>
         </div>
         <button onClick={searchData}>search</button>
       </form>
@@ -50,15 +52,16 @@ function App() {
           dataapi && (
             <div className='popup-message'>
               <div className='container2'>
-                <h3>name : {          dataapi.name}</h3>
+                <h3>name : { dataapi.name}</h3>
                 <h3>temp : {dataapi.temp}</h3>
                 <h3>main : {dataapi.main}</h3>
+                <h3>description : {dataapi.description}</h3>
               </div>
               <button id='cancel-button' onClick={handleClick}>x</button>
             </div>
           )          
         }
-      </div>
+    </div>
     </div>
   );
 }
